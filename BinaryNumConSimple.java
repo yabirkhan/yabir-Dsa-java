e9c7f1cca39566b4c385b40c027e6c2092815fc8
Yabir KHan <146160694+yabirkhan@users.noreply.github.com>
2025-03-21 18:21:55 +0530

    Add files via upload

---
+++ BinaryNumConSimple.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class BinaryNumConSimple {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        //System.out.println(Integer.toBinaryString(n));
+        System.out.println(Integer.toString(n,2));
+
+    }
+}
